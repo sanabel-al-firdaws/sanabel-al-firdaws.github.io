76faad70f8b99e8d9cb2259fa5289f9c097bf6df
sanabel-al-firdaws <136203274+sanabel-al-firdaws@users.noreply.github.com>
2024-07-04 05:19:48 +0300

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -15,12 +15,7 @@ export default defineConfig({
     mode: "production", ///production
     registerType: "autoUpdate",
     includeAssets: ["favicon.ico","apple-touch-icon-180x180.png", "maskable-icon-512x512.png"],
-    workbox: {
-        globPatterns: ["**/*.{css,json,js,html,svg,png,ico,txt}"],
-        skipWaiting: true,
-        clientsClaim: true,
-        cleanupOutdatedCaches: true
-      },
+    
 
       experimental: {
         directoryAndTrailingSlashHandler: true,
