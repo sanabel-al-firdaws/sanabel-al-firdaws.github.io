3fc41e542cdd017b4a9f4caeec93716e1bb8c9e7
sanabel-al-firdaws <136203274+sanabel-al-firdaws@users.noreply.github.com>
2024-07-04 16:32:51 +0300

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -181,7 +181,7 @@ export default defineConfig({
             handler: 'NetworkFirst',
           },
             {
-            urlPattern: /\.*$/,
+            urlPattern: /./,
             handler: 'NetworkFirst',
           },
             ]
