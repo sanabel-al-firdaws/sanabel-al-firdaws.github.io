fef6861577dd692908325dcbf7a8e500eaf03594
sanabel-al-firdaws <136203274+sanabel-al-firdaws@users.noreply.github.com>
2024-07-05 19:57:23 +0300

    [no ci]

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-
+import {s} from 'hastscript' 
 import AstroPWA from "@vite-pwa/astro";
 import type { ManifestOptions } from "vite-plugin-pwa";
 import manifest from "./webmanifest.json";
@@ -19,7 +19,22 @@ markdown: {
         {
           // Wrap the heading text in a link.
           behavior: 'wrap',
-          
+        
+    content: s(
+      'svg.octicon.octicon-link',
+      {
+        viewBox: '0 0 16 16',
+        version: '1.1',
+        width: 16,
+        height: 16,
+        ariaHidden: 'true'
+      },
+      s('path', {
+        d: 'm7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z'
+      })
+    ),
+    headingProperties: {tabIndex: '-1', dir: 'auto'},
+    properties: {className: ['heading-link']}
         },
       ],
     ],
