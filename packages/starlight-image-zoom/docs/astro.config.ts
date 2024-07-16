0536a7374221f86e875cc3ccef0eacd337694437
sanabel-al-firdaws <abdullhakim.sami@gmail.com>
2024-07-16 13:54:56 +0300

    test

---
+++ packages/starlight-image-zoom/docs/astro.config.ts
@@ -0,0 +1,43 @@
+import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config'
+import starlightImageZoom from 'starlight-image-zoom'
+
+export default defineConfig({
+  integrations: [
+    starlight({
+      customCss: ['./src/styles/custom.css'],
+      editLink: {
+        baseUrl: 'https://github.com/HiDeoo/starlight-image-zoom/edit/main/docs/',
+      },
+      plugins: [starlightImageZoom()],
+      sidebar: [
+        {
+          label: 'Start Here',
+          items: [
+            { label: 'Getting Started', link: '/getting-started/' },
+            { label: 'Configuration', link: '/configuration/' },
+          ],
+        },
+        {
+          label: 'Guides',
+          items: [
+            { label: 'Ignoring Images', link: '/ignoring-images/' },
+            { label: 'Customization', link: '/customization/' },
+          ],
+        },
+        {
+          label: 'Resources',
+          items: [
+            { label: 'Showcase', link: '/resources/showcase/' },
+            { label: 'Plugins and Tools', link: '/resources/starlight/' },
+          ],
+        },
+        { label: 'Demo', link: '/demo/' },
+      ],
+      social: {
+        github: 'https://github.com/HiDeoo/starlight-image-zoom',
+      },
+      title: 'Starlight Image Zoom',
+    }),
+  ],
+})
