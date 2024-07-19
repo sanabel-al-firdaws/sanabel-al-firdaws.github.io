886ffbf88c3c1bf5d4300fe97a47709517a36be3
sanabel-al-firdaws <abdullhakim.sami@gmail.com>
2024-07-19 18:31:54 +0300

    done

---
+++ scripts/docs-to-md.js
@@ -0,0 +1,30 @@
+
+import { chdir} from 'process';
+import { fetchGoogleDocsFiles } from "docs-markdown";
+
+const arabic = [
+   '1wkEV-zx0AhKC0znVgpgygHF5i4KcJRY64obmrG4JpGI:intro.md',
+   '1YOtsFhx9RjBK-xgyeSlGaEp3QlcGKDizV5eyIm6d2PQ:ajoromia.md',
+ ];
+ const aqida = [
+   '1YOtsFhx9RjBK-xgyeSlGaEp3QlcGKDizV5eyIm6d2PQ:aqida.md',
+ ];
+
+main()
+async function main() {
+     chdir('src/content/docs/arabic');
+     for (const file of arabic) {
+      await fetchGoogleDocsFiles([file]);
+     }
+     chdir('../aqida');
+     for (const file of aqida) {
+      await fetchGoogleDocsFiles([file]);
+     }
+}
+
+
+
+
+
+   
+   
