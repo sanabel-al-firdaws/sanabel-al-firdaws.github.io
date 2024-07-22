2acbbe6fc0c43716282a516e74d123510c589c9e
sanabel-al-firdaws <abdullhakim.sami@gmail.com>
2024-07-22 17:18:21 +0300

    done

--- astro.config.ts
+++ astro.config.ts
@@ -156,6 +156,7 @@ export default defineConfig({
     ///production
 
     // experimental: {
+    
     //   directoryAndTrailingSlashHandler: true
     // },
     manifest: (manifest as Partial<ManifestOptions>)
