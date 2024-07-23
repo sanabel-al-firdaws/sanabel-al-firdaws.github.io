254c64217650b4149e9093d337ecc0491fa59c0e
sanabel-al-firdaws <abdullhakim.sami@gmail.com>
2024-07-23 18:18:36 +0300

    done

--- astro.config.ts
+++ astro.config.ts
@@ -154,7 +154,7 @@ export default defineConfig({
   })
   , AstroPWA({
     mode: "production",
-    injectRegister: 'inline',
+    injectRegister: 'script',
     workbox: {
       cleanupOutdatedCaches: false,
       globPatterns: ['**/*.{js,css,png,svg,ico,ttf}'],
@@ -170,7 +170,8 @@ export default defineConfig({
         options: { 
             cacheName: 'pages',
             expiration:  {
-              maxAgeSeconds: ( 60 * 60 * 24 * 365) 
+              maxAgeSeconds: ( 60 * 60 * 24 * 365) ,
+              maxEntries: 300,
             },
             cacheableResponse: {
               statuses: [200]
