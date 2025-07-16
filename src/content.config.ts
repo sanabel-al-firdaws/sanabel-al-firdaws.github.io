9ae5b642c76c1469bdc1bcf7a20fc72c03a0df98
p2p-notes <abdullhakim.sami@gmail.com>
2025-07-16 17:49:56 +0300

    .

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,5 +3,5 @@ import { docsLoader, i18nLoader } from "@astrojs/starlight/loaders";
 import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
 export const collections = {
   docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-  i18n: defineCollection({ loader: i18nLoader(), schema: i18nSchema() }),
+  // i18n: defineCollection({ loader: i18nLoader(), schema: i18nSchema() }),
 };
